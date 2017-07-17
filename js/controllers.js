78a991b005c0feb087316c02feb8ed74ed25cf82
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-07-17 09:28:04 +0200

    Animation disabled

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,25 @@
 angular.module('myApp').controller('myCtrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
+   $scope.treeNodes =[{
+	name: "Node 1",
+        children: [{
+            name: "Node 1.1",
+            children:[
+				{name:"Node 1.1.1"},
+				{name: "Node 1.1.2"}]
+        }]
+	},{
+        name: "Node 2",
+        children: [
+			{name: "Node 2.1"},
+			{name: "Node 2.2"}
+		]
+    }];
+
+
+    $scope.options = {
+	    expandOnClick:true,
+	    showIcon: true,
+	    
+	}
+
 });
